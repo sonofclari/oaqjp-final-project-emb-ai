7e9104bf47b1bb0b3c752bf6bfa77a6154c52868
sonofclari <clark.josekarl@gmail.com>
2026-05-13 19:31:39 -0700

    Create test_emotion_detector.py

---
+++ test_emotion_detector.py
@@ -0,0 +1,15 @@
+from EmotionDetection.emotion_detection import emotion_detector 
+import unittest
+
+class TestEmotionDetector(unittest.TestCase): 
+  def test_sentiment_analyzer(self):
+    result_1 = emotion_detector('I love working with Python') 
+    self.assertEqual(result_1['label'], 'SENT_POSITIVE') 
+    
+    result_2 = emotion_detector('I hate working with Python') 
+    self.assertEqual(result_2['label'], 'SENT_NEGATIVE') 
+     
+    result_3 = emotion_detector('I am neutral on Python') 
+    self.assertEqual(result_3['label'], 'SENT_NEUTRAL')
+
+unittest.main()
